59b43604f3849420e8cd346d9e399246b84720ea
pjpawel <pawel.jan.podgorski@gmail.com>
2026-09-02 20:49:44 +0200

    deps(config): Update criterion dependency

--- crates/config/benches/lock_parser.rs
+++ crates/config/benches/lock_parser.rs
@@ -1,4 +1,5 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
 use thp_config::parse_lock;
 
 fn lock_with_unparsed_extension() -> Vec<u8> {
